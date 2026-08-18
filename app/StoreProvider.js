ab973de3f600d52d0639ae1dce2dc72f1ba8f0c5
root <root@allserver>
2026-08-18 16:17:01 +0600

    Cart persistence via localStorage - survives page refresh

--- app/StoreProvider.js
+++ app/StoreProvider.js
@@ -1,14 +1,23 @@
 'use client'
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore } from '../lib/store'
 
 export default function StoreProvider({ children }) {
   const storeRef = useRef(undefined)
   if (!storeRef.current) {
-    // Create the store instance the first time this renders
     storeRef.current = makeStore()
   }
+
+  useEffect(() => {
+    const unsubscribe = storeRef.current.subscribe(() => {
+      const state = storeRef.current.getState()
+      try {
+        localStorage.setItem('cart', JSON.stringify({ cartItems: state.cart.cartItems }))
+      } catch (e) {}
+    })
+    return () => unsubscribe()
+  }, [])
 
   return <Provider store={storeRef.current}>{children}</Provider>
 }
